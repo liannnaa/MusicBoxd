9026911a44d1035b03b8ebf63bb28c9364926bfb
liannnaa <lnp2016@nyu.edu>
2023-05-21 21:39:39 -0500

    added placeholder text

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,6 +5,7 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      <p>hi bianca :3c</p>
       <BrowserRouter>
         <Routes>
           
