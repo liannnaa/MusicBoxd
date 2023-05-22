9f279dd38bc489481d1bfc4ab7c8618ff91b4feb
liannnaa <lnp2016@nyu.edu>
2023-05-21 21:48:30 -0500

    added template for routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
+// import [PAGE NAME] from './/pages/[PAGE NAME]';
+
 function App() {
   return (
     <div className="App">
       <p>hi bianca :3c</p>
       <BrowserRouter>
         <Routes>
-          
+          {/* <Route path="/[PATH NAME]" element={<[PAGE NAME] />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
