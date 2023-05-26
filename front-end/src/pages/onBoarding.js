d24cd30141f706bff7d231fe561175a0241690e1
liannnaa <lnp2016@nyu.edu>
2023-05-25 21:35:47 -0500

    made onboard style work better on different screens

--- front-end/src/pages/onBoarding.js
+++ front-end/src/pages/onBoarding.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './onBoarding.css';
 
 import header from "../assets/start.svg";
 import logo from "../assets/logo.PNG";
 
-const OnBoarding= (props) => {
+const OnBoarding= () => {
+  const navigate = useNavigate();
+  
+  const navigateToLogin = () => {
+    navigate("/login");
+  }
+
   return (
     <div className="on-boarding">
       <img className="on-boarding-header" src={header} alt="header"/>
@@ -13,7 +20,7 @@ const OnBoarding= (props) => {
         “Track music you’ve listened to. Save those you want to see. Tell your
         friends what’s good.”
       </span>
-      <button className="on-boarding-button">
+      <button className="on-boarding-button" onClick={navigateToLogin} >
         Get Started
       </button>
     </div>
