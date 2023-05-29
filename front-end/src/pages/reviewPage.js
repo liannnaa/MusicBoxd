744f28d3e1f5ff3685da34684acc12dc95781387
liannnaa <lnp2016@nyu.edu>
2023-05-28 22:02:33 -0500

    finished review page frontend

--- front-end/src/pages/reviewPage.js
+++ front-end/src/pages/reviewPage.js
@@ -17,29 +17,38 @@ const Review = () => {
 
   return (
     <div className="review">
-      <img className="review-back" src={Placeholder} alt="arrow" onClick={navigateToAlbum} />
-      <div className="review-header">
-        <div className="review-left">
-            <span className="review-title">
-                Album
-            </span>
-            <span className="review-year">
-                2023
-            </span>
-            <span className="review-sub">
-                Give your rating
-            </span>
-            <div className="review-rate">
-                <img className="review-stars" src={Placeholder} alt="stars" />
-                <img className="review-heart" src={Placeholder} alt="heart" />
-            </div>
+        <div className="review-top">
+            <img className="review-back" src={Placeholder} alt="arrow" onClick={navigateToAlbum} />
+                <span className="review-head">
+                    Write Your Review
+                </span>
         </div>
-        <img className="review-cover" src={Placeholder} alt="album" />
-      </div>
-      <input className="review-input" placeholder="Write down your review..." type="text" />
-      <button className="review-button" onClick={navigateToProfile} >
-        Publish
-      </button>
+        <div className="review-header">
+            <div className="review-left">
+                <div className="review-name">
+                    <span className="review-title">
+                        Album
+                    </span>
+                    <span className="review-year">
+                        Year
+                    </span>
+                </div>
+                <span className="review-sub">
+                    Give your rating
+                </span>
+                <div className="review-rate">
+                    <img className="review-stars" src={Placeholder} alt="stars" />
+                    <img className="review-heart" src={Placeholder} alt="heart" />
+                </div>
+            </div>
+            <img className="review-cover" src={Placeholder} alt="album" />
+        </div>
+        <textarea className="review-input" placeholder="Write down your review..."></textarea>
+        <button className="review-button" onClick={navigateToProfile} >
+            <span className="review-button-text">
+                Publish
+            </span>
+        </button>
     </div>
   );
 };
