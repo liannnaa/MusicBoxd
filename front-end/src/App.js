cd5c2fdee33ef1c690c96511fca81dc233a7636f
liannnaa <lnp2016@nyu.edu>
2023-06-01 19:44:33 -0500

    added user review page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,15 +8,13 @@ import SignUp from './pages/signUp';
 import Homepage from './pages/homepage';
 import Profile from './pages/profile';
 import Album from './pages/album';
-import Artist from './pages/artist';
 import Reviewpage from './pages/reviewPage';
 import FriendReview from './pages/friendReviews';
-import RecentlyListened from './pages/recentListened';
-import RecentlyReviewed from './pages/recentReviewed';
 import List from './pages/list';
 import Comment from './pages/comment';
 import Albums from './pages/albums';
 import Lists from './pages/lists';
+import UserReview from './pages/userReviewed';
 
 function App() {
   return (
@@ -29,15 +27,13 @@ function App() {
           <Route path="/Home" element={<Homepage />} />
           <Route path="/Profile" element={<Profile />} />
           <Route path="/Album" element={<Album />} />
-          <Route path="/Artist" element={<Artist />} />
           <Route path="/Review" element={<Reviewpage />} />
           <Route path="/List" element={<List />} />
           <Route path="/Comment" element={<Comment />} />
           <Route path="/Albums" element={<Albums />} />
           <Route path="/Lists" element={<Lists />} />
           <Route path="/FriendReviews" element={<FriendReview />} />
-          <Route path="/RecentlyListened" element={<RecentlyListened />} />
-          <Route path="/RecentlyReviewed" element={<RecentlyReviewed />} />
+          <Route path="/UserReviews" element={<UserReview />} />
         </Routes>
       </BrowserRouter>
     </div>
